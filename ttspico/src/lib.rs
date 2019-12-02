487a93021fa3b82cfda8c29660ef06b395c1d4c3
Paolo Jovon <paolo.jovon@gmail.com>
2019-12-02 20:52:07 +0000

    Bump up version number and add copyright mention

--- ttspico/src/lib.rs
+++ ttspico/src/lib.rs
@@ -2,6 +2,7 @@
 //! Wraps [`ttspico-sys`](../ttspico_sys/index.html).
 
 // Copyright (c) 2019 Paolo Jovon <paolo.jovon@gmail.com>
+// Copyright (c) 2019 Sergio Tortosa Benedito
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
